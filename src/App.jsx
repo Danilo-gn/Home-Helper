dc986e03a1293eea7cddb4c60c81f1fb773649f8
Danilo-gn <dgndanilo@gmail.com>
2024-09-19 01:58:39 -0300

    configurações de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ const App = () => {
       <nav class=" h-1/6">
         <ul class="w-screen h-full bg-violet-950 flex flex-wrap justify-center items-center text-white">
           <li><Link class="flex flex-wrap justify-center items-center rounded-full w-40 h-12 hover:scale-110 duration-300 mx-10 bg-indigo-800" to="/">rotina</Link></li>
-          <li><Link class="flex flex-wrap justify-center items-center rounded-full w-40 h-12 hover:scale-110 duration-300 mx-10 bg-indigo-800" to="/financas">financas</Link></li>
-          <li><Link class="flex flex-wrap justify-center items-center rounded-full w-40 h-12 hover:scale-110 duration-300 mx-10 bg-indigo-800" to="/eventos">eventos</Link></li>
+          <li><Link class="flex flex-wrap justify-center items-center rounded-full w-40 h-12 hover:scale-110 duration-300 mx-10 bg-indigo-800" to="/Home-Helper/financas">financas</Link></li>
+          <li><Link class="flex flex-wrap justify-center items-center rounded-full w-40 h-12 hover:scale-110 duration-300 mx-10 bg-indigo-800" to="/Home-Helper/eventos">eventos</Link></li>
         </ul>
       </nav>
       <Outlet /> {/* Renderiza os componentes de página aqui */}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Rotina from './pages/Rotina';
 import Financas from './pages/Financas';
@@ -11,12 +11,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-      <Router>
-        <Switch path="/Home-Helper" element={<App />}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home-Helper" element={<App />}>
           <Route index element={<Rotina />} />
           <Route path="financas" element={<Financas />} />
           <Route path="eventos" element={<Eventos />} />
-        </Switch>
-      </Router>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
