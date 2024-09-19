6703aef01b0b79de572b4f9081feca6e723a3a00
Danilo-gn <dgndanilo@gmail.com>
2024-09-19 02:02:44 -0300

    configurações de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div class="h-screen">
       <nav class=" h-1/6">
         <ul class="w-screen h-full bg-violet-950 flex flex-wrap justify-center items-center text-white">
-          <li><Link class="flex flex-wrap justify-center items-center rounded-full w-40 h-12 hover:scale-110 duration-300 mx-10 bg-indigo-800" to="/Home-Helper">rotina</Link></li>
+          <li><Link class="flex flex-wrap justify-center items-center rounded-full w-40 h-12 hover:scale-110 duration-300 mx-10 bg-indigo-800" to="/Home-Helper/rotina">rotina</Link></li>
           <li><Link class="flex flex-wrap justify-center items-center rounded-full w-40 h-12 hover:scale-110 duration-300 mx-10 bg-indigo-800" to="/Home-Helper/financas">financas</Link></li>
           <li><Link class="flex flex-wrap justify-center items-center rounded-full w-40 h-12 hover:scale-110 duration-300 mx-10 bg-indigo-800" to="/Home-Helper/eventos">eventos</Link></li>
         </ul>
